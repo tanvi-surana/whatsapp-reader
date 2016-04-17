18215c347683f79ce4b551711caf984e0a970ea2
Tanvi Surana <tanvisurana20@gmail.com>
2016-04-17 16:53:11 +0530

    initial setup

---
+++ whatsappReader/upload/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from upload.models import *
+# Register your models here.
+admin.site.register(upload_data)
---
+++ whatsappReader/upload/models.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+import os
+from django.db import models
+
+# Create your models here.
+class upload_data(models.Model):
+	upload = models.FileField(upload_to='uploaded_files/')
+	def __str__(self):
+		return os.path.basename(self.upload.name)
+	class Meta:
+		verbose_name = 'file'
+		verbose_name_plural = 'files'
+  
+
+
+
+# Tag include is not about inserting something from model into your template. Is about inserting content of specified template. You need to write custom template filter which will read your file content and return it into template:
+
+# from django import template
+
+# register = template.Library()
+
+# @register.filter
+# def print_file_content(f):
+#     try:
+#         return f.read()
+#     except IOError:
+#         return ''
+# And use it into template like:
+
+# <div>{{ object.file1|print_file_content }}</div>
+# Or you can pass it through template context. Then just read file content into your view and add this content to your template context dictionary.
+
+
+
+#http://django-filer.readthedocs.org/en/latest/usage.html
